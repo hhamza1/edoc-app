7077a82237dbe3f0bdd717e558a548fe0d30bd13
unknown <hamzahmitou@gmail.com>
2020-08-24 13:09:52 +0300

    Adding top-bar component

--- src/app/components/top-bar/top-bar.component.jsx
+++ src/app/components/top-bar/top-bar.component.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import './top-bar.styles.scss';
 
 
+import {MdSearch} from 'react-icons/md';
+
+
 
 const TopBar = () => {
     return (
         <div className="top-bar">
-            
+            <div className="top-bar__input-group">
+                <input type="text" name="search" placeholder="Search..." />
+                <MdSearch />
+            </div>
         </div>
     )
 }
