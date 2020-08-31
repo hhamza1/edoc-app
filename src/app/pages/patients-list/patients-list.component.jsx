b61d19ef3dbdfb5fc94448404a762b2bd04546f6
unknown <hamzahmitou@gmail.com>
2020-08-31 12:20:07 +0300

    Added table to patient list page

--- src/app/pages/patients-list/patients-list.component.jsx
+++ src/app/pages/patients-list/patients-list.component.jsx
@@ -16,7 +16,69 @@ const PatientsList = () => {
                 <CustomBtn href="#" title="Add Patient" reverse/>
             </div>
             <div className="patients-list__list">
-                Patients list down here
+                <table className="table-list">
+                    <thead>
+                        <tr>
+                            <td>
+                                <input type="checkbox" name="patient" />
+                            </td>
+                            <td>ID</td>
+                            <td>
+                                Picture
+                            </td>
+                            <td>Full Name</td>
+                            <td>CIN</td>
+                            <td>Birth Date</td>
+                            <td>Phone Number</td>
+                            <td>Last Visit</td>
+                        </tr>
+                    </thead>
+                    {/**Body section of the list */}
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input type="checkbox" name="patient" />
+                            </td>
+                            <td>1</td>
+                            <td>
+                                <img src="#" alt="Picture" />
+                            </td>
+                            <td>Jon Snow</td>
+                            <td>BB123456</td>
+                            <td>31-10-1988</td>
+                            <td>212-555-8246</td>
+                            <td>Monday, 16th June 2020</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="checkbox" name="patient" />
+                            </td>
+                            <td>1</td>
+                            <td>
+                                <img src="#" alt="Picture" />
+                            </td>
+                            <td>Jon Snow</td>
+                            <td>BB123456</td>
+                            <td>31-10-1988</td>
+                            <td>212-555-8246</td>
+                            <td>Monday, 16th June 2020</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="checkbox" name="patient" />
+                            </td>
+                            <td>1</td>
+                            <td>
+                                <img src="#" alt="Picture" />
+                            </td>
+                            <td>Jon Snow</td>
+                            <td>BB123456</td>
+                            <td>31-10-1988</td>
+                            <td>212-555-8246</td>
+                            <td>Monday, 16th June 2020</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     );
